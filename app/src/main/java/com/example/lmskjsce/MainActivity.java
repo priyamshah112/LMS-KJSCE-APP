5e638c3450d251cdde8a9e5f2b478f5cbb4376d4
Priyam <priyam.ds@somaiya.edu>
2020-02-28 20:43:54 +0530

    done

---
+++ app/src/main/java/com/example/lmskjsce/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lmskjsce;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings websettings = myWebView.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://lms-kjsce.somaiya.edu/");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
