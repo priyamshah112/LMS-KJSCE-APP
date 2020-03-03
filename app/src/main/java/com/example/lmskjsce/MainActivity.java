cca7810d9b45c6b52eccca8f1b21b6c2853dc60d
Priyam <priyam.ds@somaiya.edu>
2020-03-03 23:19:18 +0530

    update

--- app/src/main/java/com/example/lmskjsce/MainActivity.java
+++ app/src/main/java/com/example/lmskjsce/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lmskjsce;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     WebView myWebView;
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private static final int STORAGE_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         myWebView.getSettings().setUserAgentString("LMS KJSCE");
         WebSettings websettings = myWebView.getSettings();
         websettings.setJavaScriptEnabled(true);
+        websettings.setDomStorageEnabled(true);
+        websettings.setAllowFileAccess(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
         myWebView.loadUrl("https://lms-kjsce.somaiya.edu/");
         myWebView.setWebViewClient(new WebViewClient());
 
